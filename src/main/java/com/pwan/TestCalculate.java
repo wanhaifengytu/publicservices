6780bc937a33af47c170c237cd3e19cea4ffd1c3
Patric Wan <patric.wan@sap.com>
2017-08-05 14:16:28 +0800

    changes

--- src/main/java/com/pwan/TestCalculate.java
+++ src/main/java/com/pwan/TestCalculate.java
@@ -1,9 +1,35 @@
 package com.pwan;
 
+import java.util.Vector;
+
+import org.apache.axis.client.Call;
+import org.apache.axis.client.Service;
+
+
 public class TestCalculate {
 
 	public static void main(String[] args) {
 		System.out.println("This is the start of test apps");
+		
+		try {
+			   
+			   String endpoint = "http://www.webxml.com.cn/WebServices/WeatherWebService.asmx?wsdl";
+			   Service service = new Service();
+			   Call call = service.createCall();
+			   
+			   // 设置service所在URL
+			   call.setTargetEndpointAddress(new java.net.URL(endpoint));
+			   call.setOperationName(new QName("http://WebXml.com.cn/", "getWeatherbyCityName"));
+			   call.addParameter(new QName("http://WebXml.com.cn/","theCityName"),org.apache.axis.encoding.XMLType.XSD_STRING, javax.xml.rpc.ParameterMode.IN);
+			   call.setUseSOAPAction(true);
+			   call.setReturnType(org.apache.axis.encoding.XMLType.SOAP_VECTOR); 
+			   call.setSOAPActionURI("http://WebXml.com.cn/getWeatherbyCityName");
+			   Vector ret =  (Vector) call.invoke(new Object[]{"大庆"});
+			   System.out.println("--------"+ret);
+			  } catch (Exception e) {
+			   // TODO Auto-generated catch block
+			   e.printStackTrace();
+			  }
 
 	}
 
